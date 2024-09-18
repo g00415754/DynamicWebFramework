758c81c927fb71c77cb5d1fdc3556b005b778c18
g00415754 <114396637+g00415754@users.noreply.github.com>
2024-09-18 10:26:49 +0000

    thing.java added

--- dynamicwebframework/src/main/java/com/example/dynamicwebframework/HelloWorldController.java
+++ dynamicwebframework/src/main/java/com/example/dynamicwebframework/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.example.dynamicwebframework;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+/*import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;*/
 
 /**
  * HelloWorldController
@@ -21,12 +23,11 @@ public class HelloWorldController {
         return "Many silly clowns look like " + thisString;
     }
 
-    /* 
+    /*
     @PostMapping(value="silly")
     public String postMethodName(@RequestBody Thing thing){
         return "The" + thing.getRobot() + " robot ate " + String.valueOf(thing) +
         " bananas and ran from a " + thing.getShark() + " shark.\n";
     }
-        */
-    
+    */
 }
---
+++ dynamicwebframework/src/main/java/com/example/dynamicwebframework/Thing.java
@@ -0,0 +1,16 @@
+package com.example.dynamicwebframework;
+
+import javax.persistence.Entity;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+public class Thing {
+    private int banana;
+    private String robot;
+    private String shark;
+}
