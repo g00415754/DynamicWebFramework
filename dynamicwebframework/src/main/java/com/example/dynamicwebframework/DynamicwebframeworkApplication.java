ffc7a7ba4610e2ec88ce17a1d926919c3cd962b8
g00415754 <114396637+g00415754@users.noreply.github.com>
2024-09-18 08:44:32 +0000

    spring extension

---
+++ dynamicwebframework/src/main/java/com/example/dynamicwebframework/DynamicwebframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamicwebframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicwebframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicwebframeworkApplication.class, args);
+	}
+
+}
---
+++ dynamicwebframework/src/test/java/com/example/dynamicwebframework/DynamicwebframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamicwebframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicwebframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
