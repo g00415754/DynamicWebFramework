b441a22c59a333ea843f7232538fa08ecc9747a4
g00415754 <114396637+g00415754@users.noreply.github.com>
2024-09-18 09:26:01 +0000

    -

--- dynamicwebframework/src/main/java/com/example/dynamicwebframework/HelloWorldController.java
+++ dynamicwebframework/src/main/java/com/example/dynamicwebframework/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.dynamicwebframework;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * HelloWorldController
@@ -14,12 +15,13 @@ public class HelloWorldController {
         return "Many silly clowns look like they know everything";
     }
 
-    /* 
+    
     @GetMapping(value="muck/(thisString)")
     public String getAKnownPicture(@PathVariable String thisString){
         return "Many silly clowns look like " + thisString;
     }
 
+    /* 
     @PostMapping(value="silly")
     public String postMethodName(@RequestBody Thing thing){
         return "The" + thing.getRobot() + " robot ate " + String.valueOf(thing) +
