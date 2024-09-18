186070f4bac65d553e261d773666a9df9954236f
g00415754 <114396637+g00415754@users.noreply.github.com>
2024-09-18 09:13:51 +0000

    can open in browser

--- dynamicwebframework/src/main/java/com/example/dynamicwebframework/HelloWorldController.java
+++ dynamicwebframework/src/main/java/com/example/dynamicwebframework/HelloWorldController.java
@@ -13,5 +13,18 @@ public class HelloWorldController {
     public String getHelloWorld(){
         return "Many silly clowns look like they know everything";
     }
+
+    /* 
+    @GetMapping(value="muck/(thisString)")
+    public String getAKnownPicture(@PathVariable String thisString){
+        return "Many silly clowns look like " + thisString;
+    }
+
+    @PostMapping(value="silly")
+    public String postMethodName(@RequestBody Thing thing){
+        return "The" + thing.getRobot() + " robot ate " + String.valueOf(thing) +
+        " bananas and ran from a " + thing.getShark() + " shark.\n";
+    }
+        */
     
 }
